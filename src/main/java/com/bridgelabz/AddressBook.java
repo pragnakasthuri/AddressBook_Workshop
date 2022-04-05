fd7491b9a63e2c235fab86a8f9fcecd438605a26
pragnakasthruri <kpragna25@gmail.com>
2022-04-05 07:24:59 +0530

    Adding the code for adding the type of address book to system

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -16,34 +16,40 @@ public class AddressBook {
     private static Scanner scanner = new Scanner(System.in);
 
     /**
-     * Creating new array list for storing the person details
+     * Creating new for hash Map storing multiple address books
      */
-    private List<Person> addressBook = new ArrayList();
+    private static Map<String, List<Person>> addressBook = new HashMap<>();
 
     /**
      * Creating addNewContact method to add contacts into list
+     *
      * @param scanner - taking scanner object
      */
-    public void addNewContact(Scanner scanner) {
+    public void readContactDetails(Scanner scanner) {
+
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Please enter the contact type  1.Office Contact\n2.Personal Contact");
+        String contactType = scanner.nextInt() == 1 ? "Office" : "Personal";
+
         System.out.println("Please enter the details :");
         String firstName = "";
         System.out.println("First Name :");
-        while(true) {
+        while (true) {
             try {
                 firstName = bufferedReader.readLine();
                 if (AddressBookValidationUtil.isValidFirstName(firstName)) {
                     break;
                 }
             } catch (AddressBookException e) {
-                System.err.println(e.getMessage()+", Please enter again");
+                System.err.println(e.getMessage() + ", Please enter again");
             } catch (IOException e) {
                 System.err.println(e.getMessage());
             }
         }
         System.out.println("Last Name :");
         String lastName = "";
-        while(true) {
+        while (true) {
             try {
                 lastName = bufferedReader.readLine();
                 if (AddressBookValidationUtil.isValidLastName(lastName)) {
@@ -57,7 +63,7 @@ public class AddressBook {
         }
         System.out.println("Address :");
         String address = "";
-        while(true) {
+        while (true) {
             try {
                 address = bufferedReader.readLine();
                 if (AddressBookValidationUtil.isValidAddress(address)) {
@@ -71,7 +77,7 @@ public class AddressBook {
         }
         System.out.println("City :");
         String city = "";
-        while(true) {
+        while (true) {
             try {
                 city = bufferedReader.readLine();
                 if (AddressBookValidationUtil.isValidCity(city)) {
@@ -85,21 +91,21 @@ public class AddressBook {
         }
         System.out.println("State :");
         String state = "";
-        while(true) {
+        while (true) {
             try {
                 state = bufferedReader.readLine();
                 if (AddressBookValidationUtil.isValidState(state)) {
                     break;
                 }
             } catch (AddressBookException e) {
-                System.err.println(e.getMessage()+ ", Please enter again");
+                System.err.println(e.getMessage() + ", Please enter again");
             } catch (IOException e) {
                 System.err.println(e.getMessage());
             }
         }
         System.out.println("Zip :");
         String zip = "";
-        while(true) {
+        while (true) {
             try {
                 zip = bufferedReader.readLine();
                 if (AddressBookValidationUtil.isValidZip(zip)) {
@@ -113,7 +119,7 @@ public class AddressBook {
         }
         System.out.println("Phone Number :");
         String phoneNumber = "";
-        while(true) {
+        while (true) {
             try {
                 phoneNumber = bufferedReader.readLine();
                 if (AddressBookValidationUtil.isValidPhoneNumber(phoneNumber)) {
@@ -127,7 +133,7 @@ public class AddressBook {
         }
         System.out.println("Email :");
         String email = "";
-        while(true) {
+        while (true) {
             try {
                 email = bufferedReader.readLine();
                 if (AddressBookValidationUtil.isValidEmail(email)) {
@@ -144,21 +150,28 @@ public class AddressBook {
          * Creating Contact object and passing all the details as params
          */
         Person person = new Person(firstName, lastName, address, city, state, zip, phoneNumber, email);
-        /**
-         * Adding the person details to address book
-         */
-        this.addNewContact(person);
+        this.addNewContact(person, contactType);
     }
+
 
     /**
      * Creating addNewContact method to address book
      * @param person
      */
-    public void addNewContact(Person person) {
-        if (person != null) {
-            this.addressBook.add(person);
+    public void addNewContact(Person person, String contactType) {
+        /**
+         * Adding the person details to address book
+         */
+        List<Person> tempContactList = null;
+        if (addressBook.get(contactType) == null) { // if there is no existing entry for given contacttype
+            tempContactList = new ArrayList<>();
+        } else {
+            tempContactList = addressBook.get(contactType);
         }
+        tempContactList.add(person);
+        addressBook.put(contactType, tempContactList);
     }
+
 
     /**
      * Creating getContact method to get the name if the given name is present
@@ -166,8 +179,12 @@ public class AddressBook {
      * @return - optional person
      * @throws AddressBookException
      */
-    public Person getContact(String personName) throws AddressBookException {
-        Optional<Person> optionalPerson = this.addressBook.stream().filter(person -> person.getFirstName().equalsIgnoreCase(personName)).findFirst();
+    public Person getContact(String personName, String contactType) throws AddressBookException {
+        List<Person> contactList = addressBook.get(contactType);
+        if (contactList == null) {
+            throw new AddressBookException("No such contact type found", AddressBookException.ExceptionType.NO_SUCH_CONTACT_TYPE_FOUND);
+        }
+        Optional<Person> optionalPerson = contactList.stream().filter(contact1 -> contact1.getFirstName().equals(personName)).findFirst();
         if (optionalPerson.isPresent()) {
             return optionalPerson.get();
         }
@@ -178,59 +195,66 @@ public class AddressBook {
      * This method is used to give the user edit option based on the first name
      */
     public void editContact() {
-        System.out.println("Enter the person name you want to edit: ");
-        String personName = scanner.next();
+        System.out.println("Please enter the contact type  1.Office Contact\n2.Personal Contact");
+        String contactType = scanner.nextInt() == 1 ? "Office" : "Personal";
+        List<Person> contactList = addressBook.get(contactType);
+        if (contactList == null) {
+            System.out.println("No such contact book exists!");
+        } else {
+            System.out.println("Enter the person name you want to edit: ");
+            String personName = scanner.next();
 
-        Optional<Person> optionalContact = addressBook.stream().filter(person -> person.getFirstName().equals(personName)).findFirst();
-        if (!optionalContact.isPresent()) {
-            System.out.println("Sorry could not find the contact with the given name");
-            return;
+            Optional<Person> optionalContact = contactList.stream().filter(contact1 -> contact1.getFirstName().equals(personName)).findFirst();
+            if (!optionalContact.isPresent()) {
+                System.out.println("Sorry could not find the contact with the given name");
+                return;
+            }
+            Person person = optionalContact.get();
+            System.out.println("Please select the field you want to edit ");
+            System.out.println("1.Lastname  2.Address  3.City  4.State  5.Zip  6.Phone number  7.email ");
+            int userChoice = scanner.nextInt();
+            switch (userChoice) {
+                case 1:
+                    System.out.println("Please enter new last name :");
+                    String lastName = scanner.next();
+                    person.setLastName(lastName);
+                    break;
+                case 2:
+                    System.out.println("Please enter new address :");
+                    String address = scanner.next();
+                    person.setAddress(address);
+                    break;
+                case 3:
+                    System.out.println("Please enter new city :");
+                    String city = scanner.next();
+                    person.setCity(city);
+                    break;
+                case 4:
+                    System.out.println("Please enter new state :");
+                    String state = scanner.next();
+                    person.setState(state);
+                    break;
+                case 5:
+                    System.out.println("Please enter new zip :");
+                    String zip = scanner.next();
+                    person.setZip(zip);
+                    break;
+                case 6:
+                    System.out.println("Please enter new phone number :");
+                    String phoneNumber = scanner.next();
+                    person.setPhoneNumber(phoneNumber);
+                    break;
+                case 7:
+                    System.out.println("Please enter new email :");
+                    String email = scanner.next();
+                    person.setEmail(email);
+                    break;
+                default:
+                    System.out.println("Invalid option. Please enter valid !");
+                    scanner.close();
+            }
+            listContacts();
         }
-        Person person = optionalContact.get();
-        System.out.println("Please select the field you want to edit ");
-        System.out.println("1.Lastname  2.Address  3.City  4.State  5.Zip  6.Phone number  7.email ");
-        int userChoice = scanner.nextInt();
-        switch (userChoice) {
-            case 1:
-                System.out.println("Please enter new last name :");
-                String lastName = scanner.next();
-                person.setLastName(lastName);
-                break;
-            case 2:
-                System.out.println("Please enter new address :");
-                String address = scanner.next();
-                person.setAddress(address);
-                break;
-            case 3:
-                System.out.println("Please enter new city :");
-                String city = scanner.next();
-                person.setCity(city);
-                break;
-            case 4:
-                System.out.println("Please enter new state :");
-                String state = scanner.next();
-                person.setState(state);
-                break;
-            case 5:
-                System.out.println("Please enter new zip :");
-                String zip = scanner.next();
-                person.setZip(zip);
-                break;
-            case 6:
-                System.out.println("Please enter new phone number :");
-                String phoneNumber = scanner.next();
-                person.setPhoneNumber(phoneNumber);
-                break;
-            case 7:
-                System.out.println("Please enter new email :");
-                String email = scanner.next();
-                person.setEmail(email);
-                break;
-            default:
-                System.out.println("Invalid option. Please enter valid !");
-                scanner.close();
-        }
-        listContacts();
     }
 
     /**
@@ -241,7 +265,7 @@ public class AddressBook {
     public void readUserInput(int userChoice)throws AddressBookException{
         switch (userChoice) {
             case 1:
-                addNewContact(scanner);
+                readContactDetails(scanner);
                 System.out.println("Contact added successfully!");
                 break;
             case 2:
@@ -264,11 +288,8 @@ public class AddressBook {
      * Displaying the Person's Contact list
      */
     public void listContacts() {
-        if (this.addressBook.isEmpty()) {
-            System.out.println("No contacts found!");
-        }
-        for (Person person : addressBook) {
-            System.out.println(person);
+        for(Map.Entry<String, List<Person>> entry : addressBook.entrySet()) {
+            System.out.println(entry.getKey()+"\n"+entry.getValue());
         }
     }
 
@@ -276,21 +297,24 @@ public class AddressBook {
      * Method for deleting the contact for the given name as input
      */
     public void deleteContact() {
-        System.out.println("Please enter the name of the person u want to delete :");
-        String contactName = scanner.next();
-        Iterator<Person> iterator = addressBook.iterator();
-        while (iterator.hasNext()) {
-            Person person = iterator.next();
-            /**
-             * Checking if the gaven name is in match with the name in the list
-             */
-            if (contactName.equals(person.getFirstName())) {
-                iterator.remove();
-                System.out.println("The contact got deleted");
-            } else
-                System.out.println("Please enter the correct name: ");
+        System.out.println("Please enter the contact type  1.Office Contact\n2.Personal Contact");
+        String contactType = scanner.nextInt() == 1 ? "Office" : "Personal";
+        List<Person> contactList = addressBook.get(contactType);
+        if (contactList == null) {
+            System.out.println("No such contact book exists!");
+        } else {
+            System.out.println("Please enter the name of the person u want to delete :");
+            String contactName = scanner.next();
+            Iterator<Person> iterator = contactList.iterator();
+            while (iterator.hasNext()) {
+                Person person = iterator.next();
+                if (contactName.equals(person.getFirstName())) {
+                    iterator.remove();
+                }
+            }
         }
     }
+
 
     /**
      * Main method for manipulating person class
